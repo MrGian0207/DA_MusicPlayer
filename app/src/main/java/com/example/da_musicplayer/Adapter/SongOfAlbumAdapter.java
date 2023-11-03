81534c63212639e57f306ff7cfe3134f80c848c4
Minh Hieu <hieutranminh61@gmail.com>
2023-11-04 03:35:50 +0700

    Update Favorite Song

--- app/src/main/java/com/example/da_musicplayer/Adapter/SongOfAlbumAdapter.java
+++ app/src/main/java/com/example/da_musicplayer/Adapter/SongOfAlbumAdapter.java
@@ -2,7 +2,9 @@ package com.example.da_musicplayer.Adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +15,7 @@ import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.da_musicplayer.Define.Albums;
 import com.example.da_musicplayer.Define.Songs_Item;
-import com.example.da_musicplayer.Fragment.YourLibraryFragment;
 import com.example.da_musicplayer.MainActivity;
 import com.example.da_musicplayer.R;
 import com.google.firebase.database.DatabaseReference;
@@ -30,6 +30,8 @@ public class SongOfAlbumAdapter extends RecyclerView.Adapter<SongOfAlbumAdapter.
     private DatabaseReference databaseReference;
     private ArrayList<Songs_Item> songs_of_album_list;
     private Context mContext;
+    private SharedPreferences sharedPreferences;
+    boolean isFavorite;
 
     public interface OnItemClickListener {
         void onItemClick(int position);
@@ -41,11 +43,7 @@ public class SongOfAlbumAdapter extends RecyclerView.Adapter<SongOfAlbumAdapter.
         this.onItemClickListener = listener;
     }
 
-    public SongOfAlbumAdapter(Context mContext, ArrayList<Songs_Item> songs_of_album_list) {
-        this.songs_of_album_list = songs_of_album_list;
-        this.mContext = mContext;
-    }
-//////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////
     public SongOfAlbumAdapter(ArrayList<Songs_Item> songs_of_album_list) {
         this.songs_of_album_list = songs_of_album_list;
     }
@@ -54,46 +52,78 @@ public class SongOfAlbumAdapter extends RecyclerView.Adapter<SongOfAlbumAdapter.
         this.songs_of_album_list = filteredList;
         notifyDataSetChanged();
     }
-/////////////////////////////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////////////////////////
+    public SongOfAlbumAdapter(Context mContext, ArrayList<Songs_Item> songs_of_album_list) {
+        this.songs_of_album_list = songs_of_album_list;
+        this.mContext = mContext;
+        this.sharedPreferences = mContext.getSharedPreferences("FavoriteSongs", Context.MODE_PRIVATE);
+    }
 
     @NonNull
     @Override
     public SongOfAlbumAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.song_of_album_layout,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.song_of_album_layout, parent, false);
         return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull SongOfAlbumAdapter.MyViewHolder holder, int position) {
-        Songs_Item song_favourite = new Songs_Item(songs_of_album_list.get(position).getId(),
-                songs_of_album_list.get(position).getSource_photo(),
-                songs_of_album_list.get(position).getTitle_song(),
-                songs_of_album_list.get(position).getSinger_song(),
-                songs_of_album_list.get(position).getLink_song());
+        Songs_Item song_favourite = songs_of_album_list.get(position);
+
+        // Trạng thái ban đầu
+        isFavorite = sharedPreferences.getBoolean(song_favourite.getTitle_song(), false);
+
+        if (isFavorite) {
+            database = FirebaseDatabase.getInstance();
+            databaseReference = database.getReference();
+            databaseReference.child("Users")
+                    .child(MainActivity.uid_User())
+                    .child("listSongFavourite")
+                    .child(song_favourite.getTitle_song())
+                    .setValue(song_favourite);
+            // Đánh dấu là yêu thích và đặt nền
+            Drawable favoriteDrawable = ContextCompat.getDrawable(mContext, R.drawable.baseline_favorite_24);
+            holder.favorite_song_btn.setBackground(favoriteDrawable);
+        } else {
+            database = FirebaseDatabase.getInstance();
+            databaseReference = database.getReference();
+            databaseReference.child("Users")
+                    .child(MainActivity.uid_User())
+                    .child("listSongFavourite")
+                    .child(song_favourite.getTitle_song())
+                    .removeValue();
+            // Bỏ đánh dấu và đặt nền
+            Drawable favoriteDrawable = ContextCompat.getDrawable(mContext, R.drawable.baseline_favorite_border_24);
+            holder.favorite_song_btn.setBackground(favoriteDrawable);
+        }
 
         Picasso.get().load(songs_of_album_list.get(position).getSource_photo()).into(holder.image_songofalbum);
-        holder.singer_songofalbum.setText(songs_of_album_list.get(position).getSinger_song());
-        holder.title_songofalbum.setText(songs_of_album_list.get(position).getTitle_song());
+        holder.singer_songofalbum.setText(song_favourite.getSinger_song());
+        holder.title_songofalbum.setText(song_favourite.getTitle_song());
+
         holder.favorite_song_btn.setOnClickListener(new View.OnClickListener() {
-            Drawable buttonDrawable;
-            Drawable.ConstantState buttonDrawableState;
-            Drawable drawableToCompare = ContextCompat.getDrawable(mContext, R.drawable.baseline_favorite_24);
-            boolean isFavorite = false;
             @Override
             public void onClick(View v) {
-                buttonDrawable = holder.favorite_song_btn.getBackground();
-                buttonDrawableState = buttonDrawable.getConstantState();
-                if(!isFavorite){
+                // Cập nhật trạng thái của isFavorite tại đây
+                isFavorite = !isFavorite;
+
+                if (isFavorite) {
                     database = FirebaseDatabase.getInstance();
                     databaseReference = database.getReference();
                     databaseReference.child("Users")
-                                     .child(MainActivity.uid_User())
-                                     .child("listSongFavourite")
-                                     .child(song_favourite.getTitle_song())
-                                     .setValue(song_favourite);
+                            .child(MainActivity.uid_User())
+                            .child("listSongFavourite")
+                            .child(song_favourite.getTitle_song())
+                            .setValue(song_favourite);
+
+                    // Bạn đã đánh dấu là yêu thích
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean(song_favourite.getTitle_song(), true);
                     Drawable favoriteDrawable = ContextCompat.getDrawable(mContext, R.drawable.baseline_favorite_24);
                     holder.favorite_song_btn.setBackground(favoriteDrawable);
-                    isFavorite = true;
+                    editor.apply();
+
+                    // Thực hiện các xử lý khi đánh dấu là yêu thích
                 } else {
                     database = FirebaseDatabase.getInstance();
                     databaseReference = database.getReference();
@@ -102,9 +132,14 @@ public class SongOfAlbumAdapter extends RecyclerView.Adapter<SongOfAlbumAdapter.
                             .child("listSongFavourite")
                             .child(song_favourite.getTitle_song())
                             .removeValue();
+                    // Bỏ đánh dấu là yêu thích
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean(song_favourite.getTitle_song(), false);
                     Drawable favoriteDrawable = ContextCompat.getDrawable(mContext, R.drawable.baseline_favorite_border_24);
                     holder.favorite_song_btn.setBackground(favoriteDrawable);
-                    isFavorite = false;
+                    editor.apply();
+
+                    // Thực hiện các xử lý khi bỏ đánh dấu là yêu thích
                 }
             }
         });
@@ -115,11 +150,12 @@ public class SongOfAlbumAdapter extends RecyclerView.Adapter<SongOfAlbumAdapter.
         return songs_of_album_list.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder{
+    class MyViewHolder extends RecyclerView.ViewHolder {
         ImageView image_songofalbum;
         TextView title_songofalbum;
         TextView singer_songofalbum;
         ImageView favorite_song_btn;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             image_songofalbum = itemView.findViewById(R.id.image_songofalbum);
@@ -135,7 +171,6 @@ public class SongOfAlbumAdapter extends RecyclerView.Adapter<SongOfAlbumAdapter.
                     }
                 }
             });
-
         }
     }
 }
